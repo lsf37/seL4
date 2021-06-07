5d896a75f34df6d3a0fed70c42e75fd1cf8f5419
Axel Heider <axel.heider@hensoldt-cyber.de>
2021-06-07 02:30:18 +0200

    risc-v: remove unused L2 cache functions
    The L2 cache handling functions were copied from the ARM code in the
    initial port, but they are not used on RISC-V. Remove them from the
    code base, they can be brought back if a platform has an L2 cache that
    needs to be maintained.
    
    Signed-off-by: Axel Heider <axel.heider@hensoldt-cyber.de>

--- include/arch/riscv/arch/machine.h
+++ include/arch/riscv/arch/machine.h
@@ -224,8 +224,6 @@ void map_kernel_devices(void);
 static inline void setInterruptMode(irq_t irq, bool_t levelTrigger, bool_t polarityLow) { }
 /** MODIFIES: [*] */
 void initTimer(void);
-/* L2 cache control */
-void initL2Cache(void);
 void initLocalIRQController(void);
 void initIRQController(void);
 void setIRQTrigger(irq_t irq, bool_t trigger);
@@ -242,11 +240,6 @@ static inline void arch_pause(void)
 }
 
 #endif
-void plat_cleanL2Range(paddr_t start, paddr_t end);
-
-void plat_invalidateL2Range(paddr_t start, paddr_t end);
-
-void plat_cleanInvalidateL2Range(paddr_t start, paddr_t end);
 
 /* Update the value of the actual register to hold the expected value */
 static inline void Arch_setTLSRegister(word_t tls_base)
